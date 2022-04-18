15ebb1d118686be6efe49e18f29df4a61ec6b6a7
PriyankaVanguri <priyankav.ziraff@gmail.com>
2022-04-18 20:15:31 +0530

    Commit for flight Booking

--- UserAPIServices/Services/UserService.cs
+++ UserAPIServices/Services/UserService.cs
@@ -2,10 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserAPIServices.Context;
+using UserAPIServices.Entities;
+using UserAPIServices.Models;
 
 namespace UserAPIServices.Services
 {
     public class UserService:IUserService
     {
+        private readonly UserContext _userContext;
+        public UserService(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+        public FlightBookingModel SaveFlightBooking(FlightBookingModel flightBookingModel)
+        {
+            try
+            {
+                FlightBooking flightBooking = _userContext.FlightBooking.Where(c => c.Id == flightBookingModel.Id).FirstOrDefault();
+                if (flightBooking != null)
+                {
+                    FillFlightBookingModelToEntity(flightBookingModel, flightBooking);
+                }
+                else
+                {
+                    flightBooking = new FlightBooking();
+                    flightBooking.Id = Guid.NewGuid();
+                    flightBooking.Status = true;
+                    flightBookingModel.Id = flightBooking.Id;
+                    FillFlightBookingModelToEntity(flightBookingModel, flightBooking);
+
+                }
+                return flightBookingModel;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static void FillFlightBookingModelToEntity(FlightBookingModel flightBookingModel, FlightBooking flightBooking)
+        {
+            flightBooking.AirlineId = flightBookingModel.AirlineId;
+            flightBooking.ContactNumber = flightBookingModel.ContactNumber;
+            flightBooking.FlightId = flightBookingModel.FlightId;
+            flightBooking.FlightNumber = flightBookingModel.FlightNumber;
+            flightBooking.FromDate = flightBookingModel.FromDate;
+            flightBooking.FromLocation = flightBookingModel.FromLocation;
+
+            flightBooking.MailId = flightBookingModel.MailId;
+            flightBooking.NonVeg = flightBookingModel.NonVeg;
+            flightBooking.NoOfBUSeats = flightBookingModel.NoOfBUSeats;
+            flightBooking.NoOfNONBUSeats = flightBookingModel.NoOfNONBUSeats;
+            flightBooking.PNRNumber = flightBookingModel.PNRNumber;
+            flightBooking.Price = flightBookingModel.Price;
+            flightBooking.Remarks = flightBookingModel.Remarks;
+            flightBooking.SeatNo = flightBookingModel.SeatNo;
+
+            flightBooking.ToDate = flightBookingModel.ToDate;
+            flightBooking.ToLocation = flightBookingModel.ToLocation;
+            flightBooking.UserRegistrestionId = flightBookingModel.UserRegistrestionId;
+            flightBooking.Veg = flightBookingModel.Veg;
+        }
     }
 }
